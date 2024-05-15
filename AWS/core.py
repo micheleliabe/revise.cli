522fbce0b3a19cd0e0a700af2fb3a66c3529f462
micheleliabe <michel.eliabe.d@gmail.com>
2024-05-15 08:15:53 -0300

    update core.py

--- AWS/core.py
+++ AWS/core.py
@@ -23,11 +23,20 @@ def costs(regions: Annotated[str, typer.Option(help='A string with the list of r
     snapshot_retention = config["finders"]["aws"]["costs"]["oldSnapshots"]["daysOfRetention"]
     aws_cost_checker = AWSCostChecker(regions)
     
-    aws_cost_checker.get_gp2_volumes()
-    aws_cost_checker.get_volumes_on_stopped_instances()
-    aws_cost_checker.get_detached_volumes()
-    aws_cost_checker.get_detached_ips()    
-    aws_cost_checker.get_old_snapshots(snapshot_retention)
+    if config["finders"]["aws"]["costs"]["gp2Volumes"] == True:
+        aws_cost_checker.get_gp2_volumes()
+        
+    if config["finders"]["aws"]["costs"]["volumesOnStoppedInstances"] == True:    
+        aws_cost_checker.get_volumes_on_stopped_instances()
+    
+    if config["finders"]["aws"]["costs"]["detachedVolumes"] == True:
+        aws_cost_checker.get_detached_volumes()
+
+    if config["finders"]["aws"]["costs"]["detachedIps"] == True:
+        aws_cost_checker.get_detached_ips()    
+    
+    if config["finders"]["aws"]["costs"]["oldSnapshots"]["enabled"] == True:
+        aws_cost_checker.get_old_snapshots(snapshot_retention)
 
 @app.command()
 def security(regions: Annotated[str, typer.Option(help='A string with the list of regions to scan. Exemple: "us-east-1 us-east-2 sa-east-1"')] = "all"):
@@ -35,9 +44,16 @@ def security(regions: Annotated[str, typer.Option(help='A string with the list o
     Retrieves security recommendations for the specified AWS regions.
     """
     aws_security_checker = AWSSecurityChecker(regions)
-    aws_security_checker.get_security_groups_public_egress()
-    aws_security_checker.get_buckets_not_public_acess_block()
-    aws_security_checker.get_rds_instance_publicly_accessible()
+    
+    if config["finders"]["aws"]["security"]["insecureSecurityGroups"] == True:    
+        aws_security_checker.get_security_groups_public_egress()
+
+    if config["finders"]["aws"]["security"]["rdsInstancePubliclyAccessible"] == True:        
+        aws_security_checker.get_rds_instance_publicly_accessible()
+        
+    if config["finders"]["aws"]["security"]["s3BucketNoPublicAccessBlock"] == True:          
+        aws_security_checker.get_buckets_not_public_acess_block()
+        
     
 @app.command()
 def get(resource, regions: Annotated[str, typer.Option(help='A string with the list of regions to scan. Exemple: "us-east-1 us-east-2 sa-east-1"')] = "all"):
